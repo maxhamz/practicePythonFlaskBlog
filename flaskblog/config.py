0d0171b0944f1fc0cd2b446aaf9a601900ed8add
maxhamz <maxwell.hamzah@gmail.com>
2020-06-25 14:51:09 +0700

    used python-dotenv and .env files instead of bash_profile

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,11 +1,16 @@
 import os
+from dotenv import load_dotenv
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    load_dotenv()
+
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = os.getenv('EMAIL_USER')
+    MAIL_PASSWORD = os.getenv('EMAIL_PASS')
+    FLASK_ENV = os.getenv('FLASK_ENV')
